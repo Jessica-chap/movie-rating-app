48ceb9fe6ce8e28b160d8418954d9a4602760c6e
Jessica Chapdelaine <jessicachap223@gmail.com>
2021-03-24 15:58:56 +0000

    Created Class- User and Movie

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 """Models for movie ratings app."""
 
+
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -17,6 +18,37 @@ def connect_to_db(flask_app, db_uri='postgresql:///ratings', echo=True):
     db.init_app(flask_app)
 
     print('Connected to the db!')
+
+
+class User(db.Model):
+    """A user."""
+
+    __tablename__ = 'users'
+
+    user_id = db.Column(db.Integer,
+                        autoincrement= True,
+                        primary_key= True)
+    email = db.Column(db.String, unique= True)
+    password = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} email={self.email}>'
+
+class Movie(db.Model):
+    """Movie titles."""
+
+    __tablename__ = 'movies'
+
+    movie_id = db.Column(db.Integer,
+                        autoincrement= True,
+                        primary_key= True)
+    title = db.Column(db.String)
+    overview = db.Column(db.Text)
+    release_date = db.Column(db.DateTime)
+    poster_path = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Movie movie_id={self.movie_id} title={self.title}>'
 
 
 if __name__ == '__main__':

14c81a300a9e9bdf9975aa50e8fa26401e7d77aa
Jessica Chapdelaine <jessicachap223@gmail.com>
2021-03-24 20:45:21 +0000

    End of first section

--- model.py
+++ model.py
@@ -2,6 +2,7 @@
 
 
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -31,6 +32,8 @@ class User(db.Model):
     email = db.Column(db.String, unique= True)
     password = db.Column(db.String)
 
+     # ratings = a list of Rating objects
+
     def __repr__(self):
         return f'<User user_id={self.user_id} email={self.email}>'
 
@@ -47,8 +50,29 @@ class Movie(db.Model):
     release_date = db.Column(db.DateTime)
     poster_path = db.Column(db.String)
 
+     # ratings = a list of Rating objects
+
     def __repr__(self):
         return f'<Movie movie_id={self.movie_id} title={self.title}>'
+
+class Rating(db.Model):
+    """A movie rating."""
+
+    __tablename__ = 'ratings'
+
+    rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    score = db.Column(db.Integer)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+
+    movie = db.relationship('Movie', backref='ratings')
+    user = db.relationship('User', backref='ratings')
+
+
+    def __repr__(self):
+        return f'<Rating rating_id={self.rating_id} score={self.score}>'
+
+
 
 
 if __name__ == '__main__':
